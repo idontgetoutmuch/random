f6b315981a55c20ed01a07af066dfc4d492f9766
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-04-07 14:58:41 +0200

    Remove unnecessary functions

--- System/Random.hs
+++ System/Random.hs
@@ -959,10 +959,9 @@ runSTGen_ g action = fst $ runSTGen g action
 type StdGen = SM.SMGen
 
 instance RandomGen StdGen where
-  next = SM.nextInt
-  split = SM.splitSMGen
   genWord32 = SM.nextWord32
   genWord64 = SM.nextWord64
+  split = SM.splitSMGen
 
 {- |
 The function 'mkStdGen' provides an alternative way of producing an initial
