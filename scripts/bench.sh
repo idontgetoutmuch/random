0896141909781878621568c1ea9fa38305f7a83c
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-04-27 14:38:53 +0200

    Add usage

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,4 +1,10 @@
 #! /usr/bin/env bash
+
+# Usage: bench.sh
+#
+# Runs random:bench and outputs the results in a CSV file with the name
+# "<git branch>-<git commit>.csv".
+
 set -euo pipefail
 
 OUTPUT="$(git branch --show-current)-$(git rev-parse --short HEAD).csv"
--- scripts/compare.py
+++ scripts/compare.py
@@ -1,5 +1,12 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i python3 -p python3 python37Packages.pandas
+
+# Usage: compare.py <reference.csv> <results.csv>
+#
+# Compares the benchmark results in the second argument with those in the first
+# argument. Outputs a comparison of those rows where the results differ
+# significantly.
+
 import pandas as pd
 
 # Threshold for the speedup or slowdown factor
