1d2b5eaeaa1f7d939641806c672075d63d017c69
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-04-28 11:15:16 +0200

    Formatting

--- scripts/compare.py
+++ scripts/compare.py
@@ -12,26 +12,48 @@ import pandas as pd
 # Threshold for the relative difference, as a percentage
 SIGNIFICANT = 20
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from sys import argv
 
     ref = pd.read_csv(argv[1])
     res = pd.read_csv(argv[2])
-    comp = pd.merge(ref, res, on='Name', how='outer', validate='one_to_one', suffixes=('_ref', '_res'))
 
-    speedup_col = 'Diff'
-    comp[speedup_col] = (comp['Mean_ref'] - comp['Mean_res']) / comp['Mean_ref']
+    # v1.1 benchmarks call it 'randomR', newer ones 'uniformR'. They are
+    # directly comparable. We normalise to 'uniformR' here.
+    ref["Name"] = ref["Name"].str.replace("randomR", "uniformR")
+    res["Name"] = res["Name"].str.replace("randomR", "uniformR")
+    comp = pd.merge(
+        ref,
+        res,
+        on="Name",
+        how="outer",
+        validate="one_to_one",
+        suffixes=("_ref", "_res"),
+    )
 
-    cols = ['Name', 'Mean_ref', 'Mean_res', speedup_col]
+    diff_col = "Diff_rel"
+    comp[diff_col] = (comp["Mean_ref"] - comp["Mean_res"]) / comp["Mean_res"]
+
+    cols = ["Name", "Mean_ref", "Mean_res", diff_col]
     print_opts = {
-            'index': False,
-            'columns': cols,
-            'formatters': {
-                speedup_col: '{:,.0%}'.format
-            },
-        }
-    print('SLOWER')
-    print(comp[comp[speedup_col] < - SIGNIFICANT / 100].to_string(**print_opts))
+        "index": False,
+        "columns": cols,
+        "formatters": {diff_col: "{:,.0%}".format},
+    }
+    significant_percent = SIGNIFICANT / 100
 
-    print('\nFASTER')
-    print(comp[comp[speedup_col] > SIGNIFICANT / 100].to_string(**print_opts))
+    slower = comp[comp[diff_col] < -significant_percent]
+    if slower.empty:
+        print("SLOWER: none")
+    else:
+        print("SLOWER")
+        print(slower.to_string(**print_opts))
+
+    print()
+
+    faster = comp[comp[diff_col] > significant_percent]
+    if faster.empty:
+        print("FASTER: none")
+    else:
+        print("\nFASTER")
+        print(faster.to_string(**print_opts))
